71c46ff4e07844160c2889a53a1c65ff44c7ce2e
sohyun1007 <2020190221@korea.ac.kr>
2023-11-20 21:31:23 +0900

    지금까지 한거

--- pages/profile/components/ProfileImage/style.ts
+++ pages/profile/components/ProfileImage/style.ts
@@ -5,12 +5,16 @@ export const ProfileImageWrapper = styled.div`
     height: 10rem; /* 디자인에 맞게 크기 조정 */
     overflow: hidden;
     border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     background-color: #D9D9D9;
 `;
 
-// export const ProfileImage = styled(Image)`
-//     object-fit: cover;
-//     border-radius: 50%;
-//     width: 100%;
-//     height: 100%;
-// `;
+export const Container = styled.div`
+    width: 100%;
+    height: 10rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
--- pages/profile/index.page.tsx
+++ pages/profile/index.page.tsx
@@ -4,45 +4,15 @@ import BottomNav from 'components/common/BottomNav';
 import BlackBackground from 'components/common/BlackBackground';     
 import ProfileImage from './components/ProfileImage';
 import ButtonImage from "./components/Group 57.png";
+import ProfileInfoWrapper from './components/ProfileInfo';
 
 const Profile = () => {
 
   return (
     <>
       <BlackBackground>
-        <h1>Profile</h1>
-        <S.ProfileImageWrapper>
-          <ProfileImage></ProfileImage>
-        </S.ProfileImageWrapper>
-        <S.ProfileBoxWrapper>
-          <S.ProfileBox>
-            <div className='categories'>Name</div>
-            <div className='content'>채윤석</div>
-          </S.ProfileBox>
-        </S.ProfileBoxWrapper>
-        <S.ProfileBoxWrapper>
-          <S.ProfileBox>
-            <div className='categories'>User Name</div>
-            <div className='content'>EDM 테크노 광전사 비둘기</div>
-          </S.ProfileBox>
-        </S.ProfileBoxWrapper>
-        <S.ProfileBoxWrapper>
-          <S.ProfileBox>
-            <div className='categories'>Melody</div>
-            <div className='content'>EDM에 몸을 맡겨</div>
-          </S.ProfileBox>
-        </S.ProfileBoxWrapper>
-        <S.ProfileBoxWrapper>
-          <S.ProfileBox>
-            <div className='categories'>Genre</div>
-            <div className='content'>EDM</div>
-          </S.ProfileBox>
-        </S.ProfileBoxWrapper>
-        <S.ProfileOpenOptionWrapper>
-          <div className='box'>
-            <div className='categories'>공개계정으로 설정</div>
-          </div>
-        </S.ProfileOpenOptionWrapper>
+        <ProfileImage />
+        <ProfileInfoWrapper/>
         <BottomNav />
       </BlackBackground>
     </>
