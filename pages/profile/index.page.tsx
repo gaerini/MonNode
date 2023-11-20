ffecc39ff7fee60b836796b3b013896b4b304a88
sohyun1007 <2020190221@korea.ac.kr>
2023-11-20 22:09:20 +0900

    profile info

--- pages/profile/index.page.tsx
+++ pages/profile/index.page.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import * as S from "./style";
-import BottomNav from 'components/common/BottomNav';
-import BlackBackground from 'components/common/BlackBackground';     
-import ProfileImage from './components/ProfileImage';
 import ButtonImage from "./components/Group 57.png";
 import ProfileInfoWrapper from './components/ProfileInfo';
 import BottomNav from "components/common/BottomNav";
