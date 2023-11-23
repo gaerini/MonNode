0fe8e7e13bb9332454d7cbc568aaeb934bbce058
chaeys2000 <yunseoki0412@daum.net>
2023-11-23 22:12:05 +0900

    splash page

---
+++ components/common/startpage/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import MainLogoPNG from "public/assets/logo/mainlogo.png";
+import styled, { css } from "styled-components";
+import { Rotate } from "styles/animations";
+
+function MainLogo({ speed = "slow", isRotate = true }) {
+  const ReturnRotationSpeed = () => {
+    switch (speed) {
+      case "normal":
+        return "3s";
+      case "fast":
+        return "1.5s";
+      case "slow":
+        return " 5s";
+      default:
+        return "3s";
+    }
+  };
+  return (
+    <LogoContainer speed={ReturnRotationSpeed()} isRotate={isRotate}>
+      <img src={MainLogoPNG.src} />
+    </LogoContainer>
+  );
+}
+
+export default MainLogo;
+
+const LogoContainer = styled.div<{ speed: string; isRotate: boolean }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${({ isRotate, speed }) =>
+    isRotate &&
+    css`
+      animation: ${Rotate} ${speed} infinite linear;
+    `}
+`;
---
+++ pages/homepage/index.page.tsx
@@ -0,0 +1,8 @@
+import MainLogo from "components/common/startpage";
+import React from "react";
+
+function Test() {
+  return <MainLogo />;
+}
+
+export default Test;
